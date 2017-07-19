8e176032bac810032a474d8a71844ad30621d593
Massimo Bono <massimobono1@gmail.com>
2017-07-19 14:57:36 +0200

    Update withrestrict.c

--- restrict/withrestrict.c
+++ restrict/withrestrict.c
@@ -1,6 +1,6 @@
 //compile via gcc -S -O3 -o baserestrict.s baserestrict.c
 
-int increase(int* a, int* b, int* restrict c) {
+int increase(int* restrict a, int* restrict b, int* restrict c) {
   *a = *a + *c;
   *b = *b + *c;
 }
