c19872ab61ed60032702fdb19f55b79ee04d8b25
Andrea Rossi <code@redsandev.com>
2017-07-22 10:09:46 +0200

    Initial Commit

---
+++ LogUtility/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LogUtility
+//
+//  Created by Andrea Rossi on 22/07/2017.
+//  Copyright © 2017 Andrea Rossi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
